723ec867d9f176304dbc3355aa86250c7ce04937
Lakshika04 <krishikaa204@gmail.com>
2025-12-07 00:44:46 +0530

    user routes completed

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,19 @@
-import express from "expresss";
-import { getUserProfile, updateUserProfile } from "../controller/user.controller";
-import { verifyToken } from "../middleware/auth.middleware.js";
+import express from 'express'
+import verifyToken from '../middleware/auth.middleware.js'
+import { getUserProfile, updateUserProfile, followRequest, unfollowUser, getFollowers, getFollowings } from '../controller/user.controller.js'
+
+const router = express.Router()
 
 
+router.get('/profile/:userName', getUserProfile)
 
-const router = express.Router();
+router.put('/profile', verifyToken, updateUserProfile)
 
+router.put('/follow/:_id', verifyToken, followRequest)
+router.put('/unfollow/:_id', verifyToken, unfollowUser)
 
-router.get("/profile/:userName",getUserProfile);
-router.put("/update",verifyToken ,updateUserProfile)
+router.get('/followers/:userId', getFollowers)
+router.get('/following/:userId', getFollowings)
 
-export default router;
+export default router
+
